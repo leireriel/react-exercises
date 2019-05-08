3ca0e1fc798ce353ffd8617e9098581119d96563
leireriel <leirericop@gmail.com>
2019-05-08 17:11:14 +0200

    Finish HalfPage component and install node sass

--- src/HalfPage/index.js
+++ src/HalfPage/index.js
@@ -3,15 +3,14 @@ import React from 'react';
 class HalfPage extends React.Component {
   render() {
     return (
-      <div className="half-page">
-        <h1 className="name">Hi! Mi name is robot</h1>
-        <ul className="list">
-          <li className="item-list">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, provident. Sit odio dolores accusantium ut eos aliquam assumenda vel ab!</li>
-          <li className="item-list">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, provident. Sit odio dolores accusantium ut eos aliquam assumenda vel ab!</li>
-          <li className="item-list">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, provident. Sit odio dolores accusantium ut eos aliquam assumenda vel ab!</li>
-          <li className="item-list">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, provident. Sit odio dolores accusantium ut eos aliquam assumenda vel ab!</li>          
-          <li className="item-list">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, provident. Sit odio dolores accusantium ut eos aliquam assumenda vel ab!</li>
-        </ul>
+      <div className="container">
+        <div className="half-page half-page-left">
+          {this.props.children}
+          {this.props.children}
+        </div>
+        <div className="half-page half-page-right">
+          {this.props.children}
+        </div>
       </div>
     );
   }
