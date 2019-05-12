72e6f030dbdcd2b27983c3217301d1448b251331
leireriel <leirericop@gmail.com>
2019-05-12 14:43:21 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Hi! This is my repo with #reactJS learning exercises
 
+## To see the exercises in your browser:
+
+First, run
+
+```
+`npm start` or `yarn start`
+```
+
+Then, uncomment the code that you want to see on **App.js**
+
+
+For example, uncomment code for *OnionHater* component
+
+```javascript
+<h2>Exercise 3. OnionHater</h2>
+<OnionHater />
+```
+and comment code for *BlueSquare* component
+
+```javascript
+{/* <h2>Exercise 6. BlueSquare</h2>
+<BlueSquare /> */}
+```
+
 ## This is the list of exercises. Each of them is a component in src folder:
 1. ItemList
 
@@ -19,5 +43,4 @@
 
 9. 
 
-## If you want to make a suggestion, please open an Issue. 
-## Thanks! 💌
+## If you want to make a suggestion, please open an Issue.  Thanks! 💌
--- src/App.js
+++ src/App.js
@@ -12,12 +12,11 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-
         <h1>Exercises</h1>
-        {/* Exercise 1 */}
+
         <h2>Exercise 1. Itemlist</h2>
         <ItemList />
-        {/* Exercise 2 */}
+
         <h2>Exercise 2. Halfpage</h2>
         <HalfPage>
           <h3 className="name">Hi! Mi name is robot</h3>
@@ -27,22 +26,22 @@ class App extends React.Component {
             <li className="item-list">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, provident. Sit odio dolores accusantium ut eos aliquam assumenda vel ab!</li>
           </ul>
         </HalfPage>
-        {/* Exercise 3 */}
+
         <h2>Exercise 3. OnionHater</h2>
         <OnionHater />
-        {/* Exercise 4 */}
+
         <h2>Exercise 4. Destiny</h2>
         <Destiny />
-        {/* Exercise 5 */}
+
         <h2>Exercise 5. InputMirror</h2>
         <InputMirror />
-        {/* Exercise 6 */}
+
         <h2>Exercise 6. BlueSquare</h2>
         <BlueSquare />
-        {/* Exercise 7 */}
+
         <h2>Exercise 7. SpaceShips</h2>
         <SpaceShips />
-        {/* Exercise 8 */}
+
         <h2>Exercise 8. BtnBase -- Practice with Node Modules</h2>
         <BtnBase />
 
