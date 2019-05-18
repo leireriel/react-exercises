502a3fb3ebcc2ca88038fe07c487f85ffdac5e42
leireriel <leirericop@gmail.com>
2019-05-18 15:26:38 +0200

    Add card FilmsForm component

--- src/components/FilmsForm/components/Card.js
+++ src/components/FilmsForm/components/Card.js
@@ -5,11 +5,12 @@ class Card extends React.Component {
   render() {
     const { title, theme, language } = this.props.state;
     return (
-      <div className="card">
-        <h3>{title}</h3>
-        <p>{theme}</p>
-        <p>{language}</p>
-      </div>
+      title && theme && language === '' ? null :
+        <div className="card">
+          <h3>{title}</h3>
+          <p>{theme}</p>
+          <p>{language}</p>
+        </div>
     );
   }
 }
