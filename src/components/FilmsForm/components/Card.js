575470f14aa57c27a087cc3fb73d02c85277a753
leireriel <leirericop@gmail.com>
2019-05-20 08:30:29 +0200

    install react-router-dom

--- src/components/FilmsForm/components/Card.js
+++ src/components/FilmsForm/components/Card.js
@@ -7,13 +7,12 @@ class Card extends React.Component {
   render() {
     const { title, theme, language } = this.props.state;
     return (
-      title || theme || language !== null ?
+      (title || theme || language !== null) &&
         <div className="card">
           <h3 className="title__movie">{title}</h3>
           <p className="theme__movie">{theme}</p>
           <p className="language__movie">{language}</p>
         </div>
-      : null
     );
   }
 }
