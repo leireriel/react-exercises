e09f5928ba725e95c5fe63d1e3a758f68165364f
leireriel <leirericop@gmail.com>
2019-05-23 20:16:22 +0200

    Update public folder

---
+++ docs/precache-manifest.a0575bb0d8dac5603df1146ddab4f581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90580edd11da324aed4ab1dbd6a9f51e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15b58effb6906eaede4c",
+    "url": "./static/css/main.da88a641.chunk.css"
+  },
+  {
+    "revision": "f63e22f43313becea55d",
+    "url": "./static/js/2.765663a3.chunk.js"
+  },
+  {
+    "revision": "15b58effb6906eaede4c",
+    "url": "./static/js/main.5f59b394.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f00d66425fb1d592bd45314ae05c8ae5",
+    "url": "./static/media/onionKawaii.f00d6642.jpg"
+  },
+  {
+    "revision": "a2d566e52cf7fc6b7aed81566eeba39e",
+    "url": "./static/media/sheep.a2d566e5.png"
+  }
+]);
